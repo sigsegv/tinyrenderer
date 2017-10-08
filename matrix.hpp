6664dab3136b7fa589d21ba4845ca0b05ae085fa
Ryan Barr <ryanjbarr@gmail.com>
2017-10-07 22:25:50 -0700

    Switched to using matrix algebra for transformations

--- matrix.hpp
+++ matrix.hpp
@@ -1,48 +1,111 @@
 #pragma once
 
-template<typename T>
-class matrix4
+#include <cassert>
+#include <array>
+#include <initializer_list>
+
+template<typename T, unsigned kRows, unsigned kCols>
+class matrix
 {
 public:
     /**
+     * Create empty matrix
+     */
+    matrix() : m{}
+    {
+        
+    }
+    
+    /**
+     * Create matrix from initializer list. Values are set row first. Unset values are zero.
+     */
+    matrix(std::initializer_list<T> l) : m{}
+    {
+        assert(l.size() <= (kRows * kCols));
+        unsigned row = 0, col = 0;
+        for(auto v : l)
+        {
+            m[row][col] = v;
+            ++col;
+            if(col == kCols)
+            {
+                ++row;
+                col = 0;
+            }
+        }
+    }
+    
+    /**
      * Create identity matrix
      */
-    matrix4();
+    static matrix identity()
+    {
+        matrix m;
+        for(unsigned c = 0; c < kCols; ++c)
+        {
+            m[c][c] = 1.f;
+        }
+        return m;
+    }
     
-    matrix4(T a00, T a01, T a02, T a03, T a10, T a11, T a12, T a13, T a20, T a21, T a22, T a23, T a30, T a31, T a32, T a33);
+    /**
+     * @return row i
+     */
+    std::array<T, kCols>& operator[](size_t i)
+    {
+        assert(i < kRows);
+        return m[i];
+    }
     
-//    matrix4(const matrix4& other);
+    /**
+     * @return row i
+     */
+    const std::array<T, kCols>& operator[](size_t i) const
+    {
+        assert(i < kRows);
+        return m[i];
+    }
     
-//    matrix4& operator=(const matrix4& other);
+    template<unsigned kRhsRows, unsigned kRhsCols>
+    matrix<T, kRows, kRhsCols> operator*(const matrix<T, kRhsRows, kRhsCols>& rhs) const
+    {
+        if(kCols != kRhsRows) throw std::runtime_error("columns must equal other's rows");
+        matrix<T, kRows, kRhsCols> result;
+        for(unsigned r = 0; r < kRows; ++r)
+        {
+            for(unsigned c = 0; c < kRhsCols; ++c)
+            {
+                T y = T(0);
+                for(unsigned i = 0; i < kRhsRows; ++i)
+                {
+                    y += m[r][i] * rhs[i][c];
+                }
+                result[r][c] = y;
+            }
+        }
+        return result;
+    }
     
-    T (& operator[](unsigned char idx)) [4] { return m[idx]; }
-    const T (& operator[](unsigned char idx) const) [4] { return m[idx]; }
-    matrix4 operator*(const matrix4& rhs) const;
-    
-    
-protected:
 private:
-    T m[4][4];
+    std::array< std::array<T, kCols>, kRows> m;
 };
 
-//template<typename T, unsigned char kRows, unsigned char kCols>
-//class matrix
-//{
-//public:
-//    /**
-//     * Create identity matrix
-//     */
-//    matrix();
-//    
-//    matrix(const T val[kRows][kCols]);
-//    
-//    T (& operator[](unsigned char idx)) [kCols] { return m[idx]; }
-//    const T (& operator[](unsigned char idx) const) [kCols] { return m[idx]; }
-//    
-//    matrix& operator*(const matrix& rhs) const;
-//protected:
-//private:
-//    T m[kRows][kCols];
-//};
+template<typename T, unsigned kRows, unsigned kCols>
+std::ostream& operator<<(std::ostream& os, const matrix<T, kRows, kCols>& m)
+{
+    for(unsigned r = 0; r < kRows; ++r)
+    {
+        os << "[";
+        for(unsigned c = 0; c < kCols; ++c)
+        {
+            os << m[r][c];
+            if(c + 1 < kCols) os << ", ";
+        }
+        os << "]";
+        if(r + 1 < kRows) os << "\n";
+    }
+    return os;
+}
 
-void matrix_test();
+using matrix41f = matrix<float, 4, 1>;
+using matrix44f = matrix<float, 4, 4>;
