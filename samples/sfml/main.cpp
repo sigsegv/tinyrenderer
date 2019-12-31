41d3d9f00c995c4a448495185d83b49eadf2363e
sigsegv <sigsegv@users.noreply.github.com>
2019-12-31 09:24:27 -0800

    Can now rotate camera left/right

--- samples/sfml/main.cpp
+++ samples/sfml/main.cpp
@@ -1,3 +1,5 @@
+#define _USE_MATH_DEFINES
+#include <math.h>
 #include <SFML/Graphics.hpp>
 #include <shader_phong.hpp>
 
@@ -6,20 +8,25 @@ TGAImage texture;
 
 int main()
 {
-    model1.load_from_disk("assets/african_head.obj");
+    /*model1.load_from_disk("assets/african_head.obj");
     model1.load_diffuse_map_from_disk("assets/african_head_diffuse.tga");
     model1.load_normal_map_from_disk("assets/african_head_nm.tga");
-    model1.load_specular_map_from_disk("assets/african_head_spec.tga");
+    model1.load_specular_map_from_disk("assets/african_head_spec.tga");*/
+
+    model1.load_from_disk("assets/diablo3_pose.obj");
+    model1.load_diffuse_map_from_disk("assets/diablo3_pose_diffuse.tga");
+    model1.load_normal_map_from_disk("assets/diablo3_pose_nm.tga");
+    model1.load_specular_map_from_disk("assets/diablo3_pose_spec.tga");
 
     constexpr int width = 600;
     constexpr int height = 600;
 
     // lesson6
     vector3f eye = { 1.f, 1.f, 3.f };
+    //vector3f eye = { 0.7f, 1.f, 0.7f };
     vector3f centre = { 0.f, 0.f, 0.f };
     vector3f up = { 0.f, 1.f, 0.f };
 
-    gl_look_at(eye, centre, up);
     gl_view_port(width / 8, height / 8, width * 3 / 4, height * 3 / 4);
 
     TGAImage buffer(width, height, TGAImage::RGB);
@@ -29,21 +36,8 @@ int main()
     shader.gl_light_dir = { 1.f, 1.f, 1.f };
     shader.gl_light_dir.unit();
     shader.p_model = &model1;
-    shader.uniform_m = gl_viewport * gl_projection * gl_modelview;
-    shader.uniform_mit = (gl_modelview).inverse().transpose();
-    for (unsigned i = 0; i < model1.f.size(); ++i)
-    {
-        std::array<vector3f, 3> screen_coords;
-        for (unsigned j = 0; j < 3; ++j)
-        {
-            screen_coords[j] = shader.vertex(i, j);
-        }
-        gl_triangle(screen_coords, shader, buffer, zbuffer);
-    }
-
-    buffer.flip_vertically();
-    zbuffer.flip_vertically();
-    buffer.write_tga_file("out-test-01.tga");
+    
+    //buffer.write_tga_file("out-test-01.tga");
 
     sf::RenderWindow window(sf::VideoMode(width, height), "TinyRenderer");
 
@@ -56,14 +50,49 @@ int main()
     sf::Sprite sprite;
     sprite.setTexture(sftexture);
 
+    double camera_angle_deg = 0.0;
+
     while (window.isOpen())
     {
         sf::Event event;
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed) window.close();
+            
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+            {
+                camera_angle_deg += 2.0;
+            }
+            else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+            {
+                camera_angle_deg -= 2.0;
+            }
         }
+
+        buffer.clear();
+        zbuffer.clear();
+
+        const double x = std::cos(camera_angle_deg / 180.0 * M_PI);
+        const double y = std::sin(camera_angle_deg / 180.0 * M_PI);
+        eye.x = 4.0 * x;
+        eye.z = 4.0 * y;
         
+        gl_look_at(eye, centre, up);
+        shader.uniform_m = gl_viewport * gl_projection * gl_modelview;
+        shader.uniform_mit = (gl_modelview).inverse().transpose();
+        for (unsigned i = 0; i < model1.f.size(); ++i)
+        {
+            std::array<vector3f, 3> screen_coords;
+            for (unsigned j = 0; j < 3; ++j)
+            {
+                screen_coords[j] = shader.vertex(i, j);
+            }
+            gl_triangle(screen_coords, shader, buffer, zbuffer);
+        }
+
+        buffer.flip_vertically();
+        zbuffer.flip_vertically();
+
         window.clear(sf::Color::Blue);
 
         const unsigned stride = width * 4;
